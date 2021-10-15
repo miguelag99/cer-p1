51de0f5753ef3670f07ad2a7c1966322fb5d8177
miguelag99 <miguelag99@hotmail.com>
2021-10-15 16:49:05 +0200

    User y num en web

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cer-p1
+
+Para ejecutar la capa de presentación (Página Web):
+    sh exec_web.sh
+
+Para ejecutar la lógica de la aplicación (Obtención y almacenamiento de números):
+    systemctl enable elasticsearch.service 
+    sh exec_logic.sh
--- presentation_layer.py
+++ presentation_layer.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for, session, flash
+
 from data_utils import get_rand_number
 
 
@@ -7,10 +8,36 @@ app.secret_key = "ayush"
 
 @app.route('/')  
 def home():   
-    #return render_template("homepage2.html")  
-    n = get_rand_number()
-    return "El num aleatorio obtenido es: {}".format(n)
+    if 'email' in session:
+        email = session['email'] 
+        return render_template('Web_ini_logged.html',user = email)
+    else:
+        n = get_rand_number()
+        return render_template('Web_ini.html',num = n)
 
+
+@app.route('/login')  
+def login(): 
+    return render_template("login.html") 
+
+@app.route('/success',methods = ["POST"])   
+def success(): 
+  if request.method == "POST":  
+   session['email']=request.form['email']  
+   return render_template('success.html')  
+
+
+@app.route('/logout')  
+def logout(): 
+  if 'email' in session:  
+    session.pop('email',None)  
+    return render_template('logout.html');  
+
+@app.route('/profile')  
+def profile():  
+   if 'email' in session:  
+      email = session['email']  
+      return render_template('profile.html',name=email) 
 
     
 if __name__ == "__main__":

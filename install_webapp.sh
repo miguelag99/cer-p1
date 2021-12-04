d0484f0fe3b90e2fb90d48db8821ac640bf3fb40
Miguel Antunes <71324219+miguelag99@users.noreply.github.com>
2021-12-04 18:51:47 +0100

    Fixed python lib instalation

--- install_webapp.sh
+++ install_webapp.sh
@@ -3,4 +3,4 @@ sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
 
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
